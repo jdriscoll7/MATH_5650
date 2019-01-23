e2c89a832c353c07c041b1370ee636107b06c2b6
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2019-01-23 10:12:30 -0600

    Update beale_fn.py

--- homework_1/beale_fn.py
+++ homework_1/beale_fn.py
@@ -6,7 +6,7 @@ import numpy as np
 - Computes the Beale test function at a given point x.
 
 - Inputs:
-    - x:    input point
+    - x:    input point - tuple or numpy array
     - args: additional arguments (not currently used)
 - Outputs
     - f: value of function at point x
@@ -15,6 +15,16 @@ import numpy as np
 """
 def beale_fn(x, *args):
     
-    f =   (1.500 - x + (x.*y)).^2
-            + (2.250 - x + (x.*(y.^2))).^2
-            + (2.625 - x + (x.*(y.^3))).^2;
+    # Check type and convert to numpy array.
+    if type(x) is not np.ndarray:
+        x = np.asarray(x)
+    
+    # Compute value of function at point x - use np.multiply to allow for matrix x.
+    f =   np.power(1.500 - x[0] + (np.multiply(x[0], x[1]))), 2)
+        + np.power(2.250 - x[0] + (np.multiply(x[0], (np.power(x[1], 2))), 2)
+        + np.power(2.625 - x[0] + (np.multiply(x[0], (np.power(x[1], 3))), 2)
+                   
+    # Compute the gradient of function at point x. Constructs column vector using numpy operations.
+    g_0 = 
+    g_1 = 
+    g = np.array([[2*]]).T
